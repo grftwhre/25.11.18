380fa44dd8ace8c20ed041c74c20caecf1a45be5
grftwhre <44138690+grftwhre@users.noreply.github.com>
2018-11-25 18:08:48 +0300

    Add files via upload

---
+++ 6(ch)2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int prime(int a)
+{
+    for (int k=2;k*k<=a;k++)
+        if ((a%k)==0)
+         return 0;
+        else return 1;
+}
+int main()
+{
+int n;
+scanf("%d",&n);
+bool A[n];
+for (int y = 0; y<=n+2; y++)
+    A[y]=0;
+ for (int i = 2; i<n; i++)
+  {
+  for(long g = i*i; g<=n; g = g+i)
+   {
+    A[g]=1;
+   }
+  }
+for (int k=3;k<=n;k++)
+{
+if (prime[k]==0)
+{
+
+        if (prime[k+2]==0)
+    {
+         printf("%d\n",k);
+    }
+    for (int j=2;j<=k+2;j++)
+    {
+        int m=(k+2)/j;
+        if (prime[m]==0)
+            printf("%d\n",k);
+    }
+}
+}
+    return 0;
+}
