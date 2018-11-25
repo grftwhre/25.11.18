89ca2c9d887f1b03b7c777859b153758483db4af
grftwhre <44138690+grftwhre@users.noreply.github.com>
2018-11-25 15:57:42 +0300

    Add files via upload

---
+++ 3(euler).cpp
@@ -0,0 +1,44 @@
+#include <iostream> /*найти количество чисел взаимнопростых с введенным числом (функция эйлера)*/
+#include <stdio.h>
+using namespace std;
+int prime(int a)
+{
+if (a!=2)
+{
+if ((a<2)||(a%2==0))
+return 0;
+}
+for (int i=3;i*i<a;i++)
+{
+if((a%i)==0)
+{
+return 0;
+}
+}
+return 1;
+}
+int euler(int a)
+{
+    int n=1;
+    if ((a==1)||(a==2))
+     return 1;
+    if (prime(a)==0)
+        {
+            for (int k=2;k<a;k++)
+            {
+                if(((a%k)==0)&&(prime(k)==1))
+                    n*=euler(k);
+            }
+            return n;
+        }
+        else
+            return a-1;
+}
+int main()
+{
+    int n;
+    printf("enter n\n");
+    scanf("%d",&n);
+    printf("euler(n) = %d",euler(n));
+    return 0;
+}
