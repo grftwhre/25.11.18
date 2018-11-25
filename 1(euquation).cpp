2c2eb772d6cf843acee0e4750e5b9500093306e1
grftwhre <44138690+grftwhre@users.noreply.github.com>
2018-11-25 15:54:44 +0300

    Add files via upload

---
+++ 1(euquation).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+int x;
+int y=1;
+while(y!=36855)
+ {
+    x=((78487894-(2019*y))/2018);
+     if ((int)x==x)
+     {
+      if ((2018*x)+(2019*y)==78487894)
+      {
+      printf("x=%d\n",x);
+      printf("y=%d\n",y);
+      printf("\n");
+      }
+     }
+   y++;
+ }
+}
